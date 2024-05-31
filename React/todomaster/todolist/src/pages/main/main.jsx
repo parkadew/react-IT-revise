bb788ce9fc9b721f9490c114885540183ab774b8
parkadew <pkm_design@naver.com>
2024-05-31 17:13:06 +0900

    5/31

--- React/todomaster/todolist/src/pages/main/main.jsx
+++ React/todomaster/todolist/src/pages/main/main.jsx
@@ -1,5 +1,7 @@
 
 import styled from 'styled-components'
+import SignInForm from './_components/sigin-in-form'
+import SignUpForm from './_components/sign-up-form'
 /*
 컴포넌트를 나누는 기준
 
@@ -17,17 +19,68 @@ import styled from 'styled-components'
 const Main = () => {
     //컴포넌트 -> 레이아웃, 회원가입 폼, 로그인 폼, 인풋, 버튼
     // 만약에 컴포넌트 나누는게 익숙하지 않다면 -> 한번에 작성했다가 이후에 나눠도 ok
-    return <Wrapper>;)</Wrapper>
+    let formState = "SIGN-IN"
+
+    const handlePressSignTab = (tabName) => {
+        formState = tabName
+    }
+    
+
+    return <S.Wrapper>
+            <S.Container>
+                <S.Header>
+                    <S.Tab isSelected={formState === 'SIGN-IN'}
+                     onClick={() => handlePressSignTab("SIGN-IN")}>SIGN-IN</S.Tab>
+                    <S.Tab isSelected={formState === 'SIGN-UP'}
+                     onClick={() => handlePressSignTab("SIGN-IN")}
+                    >SIGN-UP</S.Tab>
+                </S.Header>
+                {
+                    formState === 'SIGN-IN' ? <SignInForm/> :  <SignUpForm/>
+                }
+            </S.Container>
+           </S.Wrapper>
 }
 
 export default Main
 
 // 컴포넌트 앞글자는 항상 대문자
 
-const Wrapper = styled.div`
+const Wrapper = styled.div` 
     height: calc(100vh - 140px);
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `
+
+const Container = styled.div`
+    width: 360px ;
+    border: 1px solid #999;
+`
+const Header = styled.header`
+    background-color: #00c7ae;
+    display: flex;
+`
+const Tab = styled.div`
+    width: 50%;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ${(props) => props.isSelected && 'backgroundColor: #e0e0e0'}
+
+    font-size: 28px;
+    padding: 16px;
+    font-weight: 600;
+    &:hover{
+        background-color: #e0e0e0;
+    }
+`
+
+const S = {
+    Wrapper,
+    Container,
+    Header,
+    Tab
+}
